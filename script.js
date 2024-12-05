db5ad8ab2a4bfd6829450ad7bf5a8d1db2fb4605
Samruddhi0312 <samruddhipachpinde70@gmail.com>
2024-12-05 21:17:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let color=prompt("enter a color 'v' 'i' 'b' 'g' 'y' 'o' 'r' " );
+    switch (color) {
+        case 'v':
+            document.getElementById('box').style.backgroundColor="violet";
+            
+            break;
+            case 'i':
+            document.getElementById('box').style.backgroundColor="indigo";
+            
+            break;
+            case 'b':
+            document.getElementById('box').style.backgroundColor="blue";
+            
+            break;
+            case 'g':
+            document.getElementById('box').style.backgroundColor="green";
+            
+            break;
+            case 'y':
+            document.getElementById('box').style.backgroundColor="yellow";
+            
+            break;
+            case 'o':
+            document.getElementById('box').style.backgroundColor="orange";
+            
+            break;
+            case 'r':
+            document.getElementById('box').style.backgroundColor="red";
+            
+            break;
+    
+        default:
+            break;
+    }
